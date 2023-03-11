a9904ee2810f3439507d04c188eca82b1e7952db
prabhu <sekarvprabhu@gmail.com>
2023-03-11 20:00:51 +0530

    initial commit

---
+++ app/src/main/java/com/vpdevs/randomquotegenerator/data/DataSource.kt
@@ -0,0 +1,30 @@
+package com.vpdevs.randomquotegenerator.data
+
+import androidx.compose.ui.graphics.Color
+import com.vpdevs.randomquotegenerator.ui.theme.Blue
+import com.vpdevs.randomquotegenerator.ui.theme.Purple
+import com.vpdevs.randomquotegenerator.ui.theme.Red
+
+object DataSource {
+
+    val dataList = listOf<String>(
+        "Every man dies. Not every man lives.",
+        "Simplicity is the ultimate sophistication.",
+        "Whatever you are, be a good one.",
+        "Limit your ‘always’ and your ‘nevers’.”",
+        "Creativity is intelligence having fun",
+        "There is no substitute for hard work",
+        "Every moment is a fresh beginning.",
+        "All limitations are self-imposed",
+        "Dream as if you’ll live forever, live as if you’ll die today.",
+        "Never regret anything that made you smile",
+        "Believe you can and you’re halfway there."
+    )
+
+    val backGroundColorList = listOf<Color>(
+        Blue ,
+        Red,
+        Purple
+    )
+
+}
---
+++ app/src/main/java/com/vpdevs/randomquotegenerator/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.vpdevs.randomquotegenerator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF000000)
+val Purple = Color(0xFF8940FE)
+val Red = Color(0xFFFF2992)
+val Blue = Color(0xFF00B8FC)
